50b0d63fa43239a4fa2dc988563e3a2de2264d66
Marc Bontje <marc.bnt@gmail.com>
2022-10-08 12:36:08 +0200

    PKBDF2 stubs

--- src/crypto/keystore.rs
+++ src/crypto/keystore.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fmt;
 
 use aes::cipher::{KeyIvInit, StreamCipher};
 use aes::Aes128;
@@ -9,9 +10,21 @@ use uuid::Uuid;
 use crate::keytools::{generate_random_bytes, get_address_from_private_key};
 use crate::util::generate_mac;
 
-use super::scrypt::get_derived_key;
+use super::scrypt;
 
 type Aes128Ctr64LE = Ctr64LE<Aes128>;
+
+#[derive(Debug)]
+pub enum KDFType {
+    PBKDF2,
+    Scrypt,
+}
+
+impl fmt::Display for KDFType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", format!("{:?}", self).to_lowercase())
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct KeyStoreV3 {
@@ -22,13 +35,15 @@ pub struct KeyStoreV3 {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "lowercase")]
 pub struct Crypto {
     cipher: String,
+    #[serde(rename = "ciphertext")]
     cipher_text: String,
     kdf: String,
     mac: String,
+    #[serde(rename = "cipherparams")]
     cipher_params: CipherParams,
+    #[serde(rename = "kdfparams")]
     kdf_params: KDFParams,
 }
 
@@ -38,12 +53,12 @@ pub struct CipherParams {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "lowercase")]
 pub struct KDFParams {
     n: u32,
     c: u32,
     r: u32,
     p: u32,
+    #[serde(rename = "dklen")]
     dk_len: u32,
     salt: String,
 }
@@ -65,13 +80,16 @@ impl KDFParams {
 pub fn encrypt_private_key(
     private_key: &[u8],
     passphrase: &[u8],
-    t: u32,
+    t: KDFType,
 ) -> Result<String, Box<dyn Error>> {
     let address = get_address_from_private_key(private_key)?;
     let iv = generate_random_bytes(16);
     let salt = generate_random_bytes(32);
 
-    let derived_key = get_derived_key(passphrase, &salt, 8192, 8, 1, 32)?;
+    let derived_key = match t {
+        KDFType::PBKDF2 => todo!(),
+        KDFType::Scrypt => scrypt::get_derived_key(passphrase, &salt, 8192, 8, 1, 32)?,
+    };
 
     let encrypt_key = &derived_key[0..16];
 
@@ -87,13 +105,13 @@ pub fn encrypt_private_key(
 
     let kp = KDFParams::new(hex::encode(salt));
 
-    let kdf = "scrypt";
-
+    dbg!(KDFType::PBKDF2.to_string());
+    dbg!(KDFType::Scrypt.to_string());
     let crypto = Crypto {
         cipher: "aes-128-ctr".to_string(),
         cipher_params: cp,
         cipher_text: hex::encode(cipher_text),
-        kdf: kdf.to_string(),
+        kdf: t.to_string(),
         kdf_params: kp,
         mac: hex::encode(mac),
     };
@@ -110,6 +128,30 @@ pub fn encrypt_private_key(
 }
 
 pub fn decrypt_private_key(json: &str, passphrase: &str) -> Result<String, Box<dyn Error>> {
+    let kv = serde_json::from_str::<KeyStoreV3>(json)?;
+
+    let cipher_text = hex::decode(&kv.crypto.cipher_text)?;
+    let iv = hex::decode(kv.crypto.cipher_params.iv)?;
+    let kdf_params = kv.crypto.kdf_params;
+    let kdf = kv.crypto.kdf;
+
+    let derived_key = scrypt::get_derived_key(
+        passphrase.as_bytes(),
+        &hex::decode(kdf_params.salt).unwrap(),
+        8192,
+        8,
+        1,
+        32,
+    )?;
+
+    let mac = generate_mac(&derived_key, &cipher_text, &iv);
+
+    dbg!(mac);
+    dbg!(kv.crypto.mac.as_bytes());
+    // if String::from_utf8(mac).unwrap().to_lowercase() != kv.crypto.mac.to_lowercase() {
+    //     return Err(String::from("Failed to decrypt.").into());
+    // }
+
     Ok("test".to_string())
 }
 
@@ -123,7 +165,7 @@ mod tests {
             &hex::decode("24180e6b0c3021aedb8f5a86f75276ee6fc7ff46e67e98e716728326102e91c9")
                 .unwrap(),
             "xiaohuo".as_bytes(),
-            0,
+            KDFType::PBKDF2,
         )
         .unwrap();
         println!("{:?}", kv);
